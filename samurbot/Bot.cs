ed76a2239ee1c3316329d8e72d9b6b9a49b43634
hideyho <egor.dunko@gmail.com>
2018-07-07 16:19:21 +0300

    create prj

---
+++ samurbot/Bot.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using TwitchLib;
+using TwitchLib.Client.Models;
+using TwitchLib.Client.Events;
+using TwitchLib.Api.Models.v5.Users;
+using TwitchLib.Client;
+using TwitchLib.Client.Extensions;
+
+namespace samurbot
+{
+    class Bot
+    {
+
+        
+        TwitchClient client;
+
+        public Bot()
+        {
+            ConnectionCredentials credentials = new ConnectionCredentials(TwitchInfo.BotUsername, TwitchInfo.BotToken);
+            client = new TwitchClient();
+            client.Initialize(credentials, TwitchInfo.ChannelName);
+
+            client.OnJoinedChannel += onJoinedChannel;
+            client.OnMessageReceived += onMessageReceived;
+            client.OnWhisperReceived += onWhisperReceived;
+            client.OnConnected += Client_OnConnected;
+
+            client.Connect();
+        }
+        private void onJoinedChannel(object sender, OnJoinedChannelArgs e)
+        {
+            Console.WriteLine("Joined");
+            client.SendMessage(e.Channel, "KonCha");
+        }
+
+        private void Client_OnConnected(object sender, OnConnectedArgs e)
+        {
+            Console.WriteLine($"Connected to {e.AutoJoinChannel}");
+        }
+
+        private void onMessageReceived(object sender, OnMessageReceivedArgs e)
+        {
+            if (e.ChatMessage.Message.Contains("ban"))
+                client.TimeoutUser(e.ChatMessage.Channel, e.ChatMessage.Username, TimeSpan.FromSeconds(5),"smth");
+        }
+
+
+        private void onWhisperReceived(object sender, OnWhisperReceivedArgs e)
+        {
+            if (e.WhisperMessage.Username == "hideyho119")
+                client.SendWhisper(e.WhisperMessage.Username, "Hello my god");
+        }
+
+      
+    }
+}
---
+++ samurbot/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace samurbot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bot bot = new Bot();
+            
+            Console.ReadLine();
+
+        }
+    }
+}
